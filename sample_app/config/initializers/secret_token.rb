f7277d5ea632d2195aa0981743b8fabb0c73b51b
Channely <qijie29896@gmail.com>
2013-10-16 16:59:47 +0800

    strong secret function

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,30 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
+#SampleApp::Application.config.secret_key_base = '6e2b484af0d994fff7c6d67be8566151861be05037892e786fa0813034e42c884ca54f0a8f47e77f75ba7e7c46af986f1321c636892fe9f4894efd71eb3cd18d'
