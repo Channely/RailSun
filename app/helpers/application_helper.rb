d18996f14c3cd8bd1d24f2eb09af12cbcc54e500
Channely <qijie29896@gmail.com>
2013-12-02 13:14:52 +0800

    ccy - change surface

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Ago a girl"
+    base_title = "ago a girl"
     if page_title.empty?
       base_title
     else
